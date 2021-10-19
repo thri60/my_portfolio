dbdb3cf978c2268e71aa5bddac3670d447d1f323
Michael <67415283+thri60@users.noreply.github.com>
2021-10-19 10:57:19 +0100

    master

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -151,7 +151,7 @@
                       type="a"
                       href="#contact"
                       push
-                      label="Contact"
+                      label="Contact Me"
                     />
                   </div>
                 </div>
@@ -578,7 +578,7 @@
 
  <!-- place QPageScroller at end of page -->
           <q-page-scroller position="bottom-right" :scroll-offset="150" :offset="[18, 18]">
-            <q-btn fab icon="keyboard_arrow_up" id="scroll" color="accent" />
+            <q-btn fab icon="keyboard_arrow_up" color="accent" />
           </q-page-scroller>
   </q-page>
 </template>
